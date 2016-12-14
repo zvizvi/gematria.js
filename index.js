f24fd35a72169a71838a4ab2f134ff5962528bd6
zvizvi <5633910@gmail.com>
2016-12-14 02:50:44 +0200

    Edit index.js

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-gematria = function(txt){
+gematria = function(txt) {
 
-	var letterWeight = [
-		'',
-		'א',
-		'ב',
-		'ג',
-		'ד',
-		'ה',
-		'ו',
-		'ז',
-		'ח',
-		'ט',
-		'י'
-	];
-	letterWeight[20] = 'כ';
-	letterWeight[30] = 'ל';
-	letterWeight[40] = 'מ';
-	letterWeight[50] = 'נ';
-	letterWeight[60] = 'ס';
-	letterWeight[70] = 'ע';
-	letterWeight[80] = 'פ';
-	letterWeight[90] = 'צ';
-	letterWeight[100] = 'ק';
-	letterWeight[200] = 'ר';
-	letterWeight[300] = 'ש';
-	letterWeight[400] = 'ת';
-	
-	letterWeight[420] = 'ך';
-	letterWeight[440] = 'ם';
-	letterWeight[450] = 'ן';
-	letterWeight[480] = 'ף';
-	letterWeight[490] = 'ץ';
+	var letterWeight = {
+		' ': 0,
+		'א': 1,
+		'ב': 2,
+		'ג': 3,
+		'ד': 4,
+		'ה': 5,
+		'ו': 6,
+		'ז': 7,
+		'ח': 8,
+		'ט': 9,
+		'י': 10,
+		'כ': 20,
+		'ל': 30,
+		'מ': 40,
+		'נ': 50,
+		'ס': 60,
+		'ע': 70,
+		'פ': 80,
+		'צ': 90,
+		'ק': 100,
+		'ר': 200,
+		'ש': 300,
+		'ת': 400,
+		'ך': 420,
+		'ם': 440,
+		'ן': 450,
+		'ף': 480,
+		'ץ': 490,
+	}
 
 
-	var getLetterWeight = function(letter){
-		var weight = letterWeight.indexOf(letter);
+	var getLetterWeight = function(letter) {
+		var weight = letterWeight[letter];
 		if (weight < 1) {
 			weight = 0;
-		}else if(weight > 400){
+		} else if (weight > 400) {
 			weight = weight - 400;
 		}
 		return weight;
 	}
 
-	this.toNumbers = function(txt){
+	this.toNumbers = function(txt) {
 		var letter = txt.replace(/\s/g, '').split('');
 		var totalWeight = 0;
-		for(l in letter){
+		for (l in letter) {
 			totalWeight += getLetterWeight(letter[l]);
 		}
 		return totalWeight;
